f68717b0e5e584f27fb3b9ad3e6cb2d0384d1e4e
Anton Stadie <antonstadie1999@gmail.com>
2021-04-09 07:50:17 +0200

    remove package locks

--- poll.js
+++ poll.js
@@ -4,9 +4,10 @@
 let { request, gql } = require("graphql-request");
 let Web3 = require("web3");
 let abi = require("./abis/abi.json");
-let currentNode = "wss://mainnet.infura.io/ws/v3/fa39b33c7d9f40f2aad8760e1ca0a26f";
+let currentNode =
+  "wss://mainnet.infura.io/ws/v3/fa39b33c7d9f40f2aad8760e1ca0a26f";
 let pairAddress = "0x9ca8aef2372c705d6848fdda3c1267a7f51267c1";
-let block
+let block;
 let poll = {
   setup() {
     this.web3 = new Web3(currentNode);
@@ -15,26 +16,30 @@ let poll = {
   async getCurrentBlock() {
     return (await this.web3.eth.getBlock("latest")).number - 5;
   },
-  async interval(){
-    setInterval(async function(){ poll.test(); }, 10000);
+  async interval() {
+    poll.test();
+    setInterval(async function () {
+      poll.test();
+    }, 10000);
   },
 
-  async test(){
+  async test() {
     block = await poll.getCurrentBlock();
     let priceInETH = await poll.getPrice();
     let ethPrice = await poll.getETHPrice();
-    let priceInUSD = priceInETH * ethPrice
+    let priceInUSD = priceInETH * ethPrice;
     let prices = {
-      "priceInUSD": priceInUSD,
-      "priceInETH": priceInETH
-    }
+      priceInUSD: priceInUSD,
+      priceInETH: priceInETH,
+    };
     console.log(prices);
   },
   async getPrice() {
-  const pairdata =
-    gql`{
-    pair(id: "0x9ca8aef2372c705d6848fdda3c1267a7f51267c1", block: { number: `+
-    block+`}) {
+    const pairdata =
+      gql`{
+    pair(id: "0x9ca8aef2372c705d6848fdda3c1267a7f51267c1", block: { number: ` +
+      block +
+      `}) {
           token0 {
             id
             symbol
@@ -58,27 +63,27 @@ let poll = {
         }
       }
     `;
-  let response = await request(
-    "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-    pairdata
+    let response = await request(
+      "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+      pairdata
     );
-  let reserve0 = parseFloat(response.pair.reserve0)
-  let reserve1 = parseFloat(response.pair.reserve1)
-  let priceInETH = reserve1/reserve0
-  return priceInETH
+    let reserve0 = parseFloat(response.pair.reserve0);
+    let reserve1 = parseFloat(response.pair.reserve1);
+    let priceInETH = reserve1 / reserve0;
+    return priceInETH;
   },
   async getETHPrice() {
-  let query = `{
+    let query = `{
       bundle(id: "1") {
         ethPrice
       },
     }`;
-  let response = await request(
-    "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-    query
-  );
-  return parseFloat(response.bundle.ethPrice);
-}
+    let response = await request(
+      "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+      query
+    );
+    return parseFloat(response.bundle.ethPrice);
+  },
 };
 
 poll.setup();
